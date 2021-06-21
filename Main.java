abc8246ff5aae3a3f9443d108f67a935dcab085c
Ryan Garrity <rgarrity7000@gmail.com>
2021-06-21 03:22:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    String str = "W.I.N.E = W.I.N.E Is Not an Emulator";
+    System.out.println(str);
+    while(true){
+      str += " : W.I.N.E = W.I.N.E Is Not an Emulator";
+      System.out.println(str);
+    }
+  }
+}
+/*
+Girlfriend
+Blender
+Dinner
+Windshield Wipers on glasses
+Sports game but not football (and of mobile quality)
+Blender image generator (generates images of blenders specifically)
+Mobile Disk Golf (Compete dk golf scores with other people (Website w/ servers and shit))
+Floating desk
+Shoe House
+A Brazilian Startup that helps fix cars via it's app
+Sterroid Ticktack toe
+A calculator for streaming videos
+Ultimate Tic TTic Taac Toec Toe
+Connect 4
+WINE Acronym Resoliver
+*/
